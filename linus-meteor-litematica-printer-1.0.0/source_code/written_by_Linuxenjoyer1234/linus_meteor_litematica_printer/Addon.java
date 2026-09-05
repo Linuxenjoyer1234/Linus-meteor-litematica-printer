c3d9424365e8ee1c772e2f5a4eb3e8c3e145f6c5
Linuxenjoyer1234 <Linuxenjoyer1234@tutamail.com>
2026-09-05 10:34:05 +0200

    Added source code.
    You can now view it.

---
+++ linus-meteor-litematica-printer-1.0.0/source_code/written_by_Linuxenjoyer1234/linus_meteor_litematica_printer/Addon.java
@@ -0,0 +1,29 @@
+package written_by_Linuxenjoyer1234.linus_meteor_litematica_printer;
+
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Addon extends MeteorAddon {
+   public static final Logger LOG = LogManager.getLogger();
+   public static final Category CATEGORY = new Category("Litematica", () -> new ItemStack(Items.PINK_CARPET));
+
+   public void onInitialize() {
+      LOG.info("Initializing linus meteor litematica printer");
+      Modules.get().add(new Printer());
+      // Modules.get().add(new Shredder());
+      // Shredder will be added in v1.0.2
+   }
+
+   public String getPackage() {
+      return "written_by_Linuxenjoyer1234.linus_meteor_litematica_printer";
+   }
+
+   public void onRegisterCategories() {
+      Modules.registerCategory(CATEGORY);
+   }
+}
